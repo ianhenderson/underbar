969627343fc925be721cb8022d35c56f0c04dcde
ianhenderson <ianstein16@gmail.com>
2014-02-09 10:43:59 +0900

    Implemented 'Filter' function.

--- src/underbar.js
+++ src/underbar.js
@@ -66,6 +66,15 @@ var _ = { };
 
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, test) {
+    var result = [];
+
+    _.each(collection, function(item, index) {
+      if (test(item)) {
+        result.push(collection[index]);
+      }
+    });
+
+    return result;
   };
 
   // Return all elements of an array that don't pass a truth test.
