adcfe5aa2b463eb106c0fd2fb77765d4124abf88
ianhenderson <ianstein16@gmail.com>
2014-02-12 22:17:01 +0900

    Implemented 'Contains' function and improved 'Reduce' function.

--- src/underbar.js
+++ src/underbar.js
@@ -149,9 +149,9 @@ var _ = { };
   //     return total + number;
   //   }, 0); // should be 6
   _.reduce = function(collection, iterator, accumulator) {
-    for (var i = 0; i < collection.length; i++){
-      accumulator = iterator(accumulator, collection[i]);
-    }
+    _.each(collection, function(value){
+      accumulator = iterator(accumulator, value);
+    });
     return accumulator;
   };
 
