1f1fc4670e016b3ad60ac69b4a9a1ca053b637a9
ianhenderson <ianstein16@gmail.com>
2014-02-17 08:08:54 +0900

    Implemented the 'Defaults' function.

--- src/underbar.js
+++ src/underbar.js
@@ -231,6 +231,14 @@ var _ = { };
   // Like extend, but doesn't ever overwrite a key that already
   // exists in obj
   _.defaults = function(obj) {
+    for (var i=0; i < arguments.length; i++){
+      for (var key in arguments[i]){
+        if (obj.hasOwnProperty(key) !== true){
+          obj[key] = arguments[i][key];
+        }
+      }
+    }
+    return obj;
   };
 
 
